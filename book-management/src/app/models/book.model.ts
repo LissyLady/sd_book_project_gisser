9d42cb155195ba992082ddf0e3d0972f5e357b52
LissyLady <64424696+LissyLady@users.noreply.github.com>
2023-09-29 10:22:10 +0200

    created book model

---
+++ book-management/src/app/models/book.model.ts
@@ -0,0 +1,9 @@
+export class Book {
+    constructor(
+        public title: string,
+        public isbn: string,
+        public pages: number,
+        public author: string,
+        public summary: string
+    ) { }
+}
